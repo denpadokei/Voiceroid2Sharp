42e90d5ba0ee7e9d2df17ac3c4d9e224cdaa8627
デンパ時計 <jsoldenpa@yahoo.co.jp>
2020-05-07 17:24:58 +0900

    Dispose実装

--- Voiceroid2Sherp/Voiceroid2.cs
+++ Voiceroid2Sherp/Voiceroid2.cs
@@ -21,7 +21,7 @@ using System.Windows;
 
 namespace Voiceroid2Sharp
 {
-    public class Voiceroid2 : BindableBase
+    public class Voiceroid2 : BindableBase, IDisposable
     {
 		//ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*ﾟ+｡｡+ﾟ*｡+ﾟ ﾟ+｡*
 		#region // プロパティ
@@ -440,6 +440,42 @@ namespace Voiceroid2Sharp
 
 			this.Messages.CollectionChanged += this.OnMessageCollectionChenged;
 		}
+
+		#region IDisposable Support
+		private bool disposedValue = false; // 重複する呼び出しを検出するには
+
+		protected virtual void Dispose(bool disposing)
+		{
+			if (!disposedValue) {
+				if (disposing) {
+					// TODO: マネージ状態を破棄します (マネージ オブジェクト)。
+					this.voiceroidEditer_.Dispose();
+					this.voiceroidProcess_.Dispose();
+				}
+
+				// TODO: アンマネージ リソース (アンマネージ オブジェクト) を解放し、下のファイナライザーをオーバーライドします。
+				// TODO: 大きなフィールドを null に設定します。
+
+				disposedValue = true;
+			}
+		}
+
+		// TODO: 上の Dispose(bool disposing) にアンマネージ リソースを解放するコードが含まれる場合にのみ、ファイナライザーをオーバーライドします。
+		// ~Voiceroid2()
+		// {
+		//   // このコードを変更しないでください。クリーンアップ コードを上の Dispose(bool disposing) に記述します。
+		//   Dispose(false);
+		// }
+
+		// このコードは、破棄可能なパターンを正しく実装できるように追加されました。
+		public void Dispose()
+		{
+			// このコードを変更しないでください。クリーンアップ コードを上の Dispose(bool disposing) に記述します。
+			Dispose(true);
+			// TODO: 上のファイナライザーがオーバーライドされる場合は、次の行のコメントを解除してください。
+			// GC.SuppressFinalize(this);
+		}
+		#endregion
 		#endregion
 	}
 }
